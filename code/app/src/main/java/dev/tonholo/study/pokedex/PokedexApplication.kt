eae02e01e8917f9052548ee44c8d26c7423cabd5
Rafael Tonholo <rafael@tonholo.dev>
2022-01-08 20:49:01 -0300

    base hilt setup

---
+++ code/app/src/main/java/dev/tonholo/study/pokedex/PokedexApplication.kt
@@ -0,0 +1,8 @@
+package dev.tonholo.study.pokedex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokedexApplication: Application() {
+}
--- code/app/src/main/java/dev/tonholo/study/pokedex/activity/MainActivity.kt
+++ code/app/src/main/java/dev/tonholo/study/pokedex/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.tonholo.study.pokedex
+package dev.tonholo.study.pokedex.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,8 +8,10 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.tonholo.study.pokedex.ui.theme.PokedexAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
