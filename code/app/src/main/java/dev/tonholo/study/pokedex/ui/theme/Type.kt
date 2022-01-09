094823fd44f4343542493128a673cd55c6413efb
Rafael Tonholo <rafael@tonholo.dev>
2022-01-09 18:42:27 -0300

    added resources

--- code/app/src/main/java/dev/tonholo/study/pokedex/ui/theme/Type.kt
+++ code/app/src/main/java/dev/tonholo/study/pokedex/ui/theme/Type.kt
@@ -2,14 +2,28 @@ package dev.tonholo.study.pokedex.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.tonholo.study.pokedex.R
+
+val Roboto = FontFamily(
+        Font(R.font.roboto_light, FontWeight.Light),
+        Font(R.font.roboto_regular, FontWeight.Normal),
+        Font(R.font.roboto_medium, FontWeight.Medium),
+        Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
+val RobotoCondensed = FontFamily(
+        Font(R.font.roboto_condensed_light, FontWeight.Light),
+        Font(R.font.roboto_condensed_regular, FontWeight.Normal),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
         body1 = TextStyle(
-                fontFamily = FontFamily.Default,
+                fontFamily = Roboto,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp
         )
