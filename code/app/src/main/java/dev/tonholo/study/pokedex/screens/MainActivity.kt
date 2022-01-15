e0b47ae599aa8fc17fe2301a697edeaa8a82f66a
Rafael Tonholo <rafael@tonholo.dev>
2022-01-15 17:48:44 -0300

    fixed status bar color

--- code/app/src/main/java/dev/tonholo/study/pokedex/screens/MainActivity.kt
+++ code/app/src/main/java/dev/tonholo/study/pokedex/screens/MainActivity.kt
@@ -1,10 +1,14 @@
 package dev.tonholo.study.pokedex.screens
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -12,6 +16,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import coil.annotation.ExperimentalCoilApi
 import dagger.hilt.android.AndroidEntryPoint
+import dev.tonholo.study.pokedex.R
 import dev.tonholo.study.pokedex.screens.pokemonDetail.PokemonDetailScreen
 import dev.tonholo.study.pokedex.screens.pokemonList.PokemonListScreen
 import dev.tonholo.study.pokedex.ui.theme.PokedexAppTheme
@@ -29,6 +34,7 @@ class MainActivity : ComponentActivity() {
                     startDestination = Routes.PokemonList.route,
                 ) {
                     composable(Routes.PokemonList.route) {
+                        window.statusBarColor = MaterialTheme.colors.background.toArgb()
                         PokemonListScreen(navController)
                     }
                     composable(
@@ -42,6 +48,7 @@ class MainActivity : ComponentActivity() {
                             }
                         ),
                     ) {
+                        window.statusBarColor = ContextCompat.getColor(this@MainActivity, android.R.color.black)
                         val pokemonName = remember {
                             it.arguments?.getString(Routes.PokemonDetails.Params.pokemonName)
                                 ?: throw IllegalArgumentException(
