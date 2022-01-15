454250480cbfed4a35d9948e4bf8104525d0e7e0
Rafael Tonholo <rafael@tonholo.dev>
2022-01-15 17:01:15 -0300

    fixed data types

--- code/app/src/main/java/dev/tonholo/study/pokedex/data/remote/responses/Sprites.kt
+++ code/app/src/main/java/dev/tonholo/study/pokedex/data/remote/responses/Sprites.kt
@@ -7,19 +7,19 @@ data class Sprites(
     @SerializedName("back_default")
     val backDefault: String,
     @SerializedName("back_female")
-    val backFemale: Any,
+    val backFemale: String,
     @SerializedName("back_shiny")
     val backShiny: String,
     @SerializedName("back_shiny_female")
-    val backShinyFemale: Any,
+    val backShinyFemale: String,
     @SerializedName("front_default")
     val frontDefault: String,
     @SerializedName("front_female")
-    val frontFemale: Any,
+    val frontFemale: String,
     @SerializedName("front_shiny")
     val frontShiny: String,
     @SerializedName("front_shiny_female")
-    val frontShinyFemale: Any,
-    val other: Other,
-    val versions: Versions,
+    val frontShinyFemale: String,
+    val other: Other? = null,
+    val versions: Versions? = null,
 )
