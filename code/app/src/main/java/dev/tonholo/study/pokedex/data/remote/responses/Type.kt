9015a3849e28bd9e10a9cf55ad27646cd95da6e1
Rafael Tonholo <rafael@tonholo.dev>
2022-01-16 13:34:36 -0300

    added extension method

--- code/app/src/main/java/dev/tonholo/study/pokedex/data/remote/responses/Type.kt
+++ code/app/src/main/java/dev/tonholo/study/pokedex/data/remote/responses/Type.kt
@@ -1,7 +1,32 @@
 package dev.tonholo.study.pokedex.data.remote.responses
 
+import androidx.compose.ui.graphics.Color
+import dev.tonholo.study.pokedex.ui.theme.*
+
 
 data class Type(
     val slot: Int,
     val type: TypeX,
 )
+
+fun Type.toColor(): Color = when (type.name.lowercase()) {
+    "normal" -> TypeNormal
+    "fire" -> TypeFire
+    "water" -> TypeWater
+    "electric" -> TypeElectric
+    "grass" -> TypeGrass
+    "ice" -> TypeIce
+    "fighting" -> TypeFighting
+    "poison" -> TypePoison
+    "ground" -> TypeGround
+    "flying" -> TypeFlying
+    "psychic" -> TypePsychic
+    "bug" -> TypeBug
+    "rock" -> TypeRock
+    "ghost" -> TypeGhost
+    "dragon" -> TypeDragon
+    "dark" -> TypeDark
+    "steel" -> TypeSteel
+    "fairy" -> TypeFairy
+    else -> Color.Black
+}
