284167154eb921c0520f4945888c619cdc658fa2
Rafael Tonholo <rafael@tonholo.dev>
2022-01-24 19:27:45 -0300

    created AppDatabase

---
+++ code/app/src/main/java/dev/tonholo/study/pokedex/data/database/AppDatabase.kt
@@ -0,0 +1,36 @@
+package dev.tonholo.study.pokedex.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import dev.tonholo.study.pokedex.data.dao.PokemonDao
+import dev.tonholo.study.pokedex.data.dao.PokemonWithTypeDao
+import dev.tonholo.study.pokedex.data.entity.Pokemon
+import dev.tonholo.study.pokedex.data.entity.PokemonType
+import dev.tonholo.study.pokedex.data.entity.PokemonWithType
+
+private const val DATABASE_NAME = "pokedex_db"
+
+@Database(
+    entities = [
+        Pokemon::class,
+        PokemonType::class,
+        PokemonWithType::class,
+    ],
+    version = 1,
+    exportSchema = false,
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun pokemonDao(): PokemonDao
+    abstract fun pokemonWithTypeDao(): PokemonWithTypeDao
+
+    companion object {
+        operator fun invoke(context: Context) =
+            Room.databaseBuilder(
+                context,
+                AppDatabase::class.java,
+                DATABASE_NAME,
+            ).build()
+    }
+}
