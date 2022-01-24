1ea6198a3dda4e700440b283dd691b6059939a8e
Rafael Tonholo <rafael@tonholo.dev>
2022-01-24 19:28:21 -0300

    mapped room on hilt

---
+++ code/app/src/main/java/dev/tonholo/study/pokedex/di/DatabaseModule.kt
@@ -0,0 +1,30 @@
+package dev.tonholo.study.pokedex.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.tonholo.study.pokedex.data.dao.PokemonDao
+import dev.tonholo.study.pokedex.data.dao.PokemonWithTypeDao
+import dev.tonholo.study.pokedex.data.database.AppDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase =
+        AppDatabase(context)
+
+    @Provides
+    fun providePokemonDao(appDatabase: AppDatabase): PokemonDao =
+        appDatabase.pokemonDao()
+
+    @Provides
+    fun providePokemonWithTypeDao(appDatabase: AppDatabase): PokemonWithTypeDao =
+        appDatabase.pokemonWithTypeDao()
+}
