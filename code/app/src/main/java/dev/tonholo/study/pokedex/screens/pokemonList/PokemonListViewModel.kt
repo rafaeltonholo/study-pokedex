fcb42ed4274117a3c55fb5185f72bffffe9b65f1
Rafael Tonholo <rafael@tonholo.dev>
2022-02-05 19:32:24 -0300

    applied paging3 to view model and component

--- code/app/src/main/java/dev/tonholo/study/pokedex/screens/pokemonList/PokemonListViewModel.kt
+++ code/app/src/main/java/dev/tonholo/study/pokedex/screens/pokemonList/PokemonListViewModel.kt
@@ -6,110 +6,53 @@ import android.graphics.drawable.Drawable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.paging.ExperimentalPagingApi
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.tonholo.study.pokedex.data.model.PokemonEntry
-import dev.tonholo.study.pokedex.usecases.CachePokemonDetailUseCase
-import dev.tonholo.study.pokedex.usecases.CachePokemonListUseCase
 import dev.tonholo.study.pokedex.usecases.GetPokemonListUseCase
-import dev.tonholo.study.pokedex.util.toTitleCase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.util.*
 import javax.inject.Inject
 
 const val PAGE_SIZE = 20
 
+@ExperimentalPagingApi
 @HiltViewModel
 class PokemonListViewModel @Inject constructor(
     val getPokemonListUseCase: GetPokemonListUseCase,
-    val cachePokemonListUseCase: CachePokemonListUseCase,
 ) : ViewModel() {
-    private var currentPage = 0
-
-    val pokemonList = mutableStateOf<List<PokemonEntry>>(listOf())
     val loadingError = mutableStateOf("")
-    val isLoading = mutableStateOf(false)
-    val isEndReached = mutableStateOf(false)
     val isSearching = mutableStateOf(false)
+    val pokemonList = getPokemonListUseCase()
 
     private var cachedPokemonList = listOf<PokemonEntry>()
 
-    init {
-        loadPokemonListPaginated()
-    }
-
-    fun loadPokemonListPaginated() {
-        viewModelScope.launch {
-            isLoading.value = true
-            val params = GetPokemonListUseCase.Params(
-                limit = PAGE_SIZE,
-                offset = currentPage * PAGE_SIZE,
-            )
-
-            when (val result = getPokemonListUseCase(params)) {
-                is GetPokemonListUseCase.Result.Success -> {
-                    with(result.data) {
-                        isEndReached.value = currentPage * PAGE_SIZE >= count
-                        val pokedexEntries = result.data.results.map { entry ->
-                            val number = if (entry.url.endsWith("/")) {
-                                entry.url.dropLast(1).takeLastWhile { it.isDigit() }
-                            } else {
-                                entry.url.takeLastWhile { it.isDigit() }
-                            }
-
-                            val url =
-                                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
-
-                            PokemonEntry(
-                                pokemonName = entry.name.toTitleCase(),
-                                imageUrl = url,
-                                number = number.toInt()
-                            )
-                        }
-
-                        // cachePokemonListUseCase(pokedexEntries)
-                        pokemonList.value += pokedexEntries
-                    }
-                    isLoading.value = false
-                    currentPage++
-                }
-                is GetPokemonListUseCase.Result.Failure -> {
-                    loadingError.value = "${result.message}\n${result.exception?.message}"
-                    isLoading.value = false
-                }
-            }
-        }
-    }
-
     fun filterPokemonList(query: String) {
-        val listToSearch = if (isSearching.value) {
-            cachedPokemonList
-        } else {
-            pokemonList.value
-        }
-
-        viewModelScope.launch(Dispatchers.Default) {
-            if (query.isBlank()) {
-                pokemonList.value = cachedPokemonList
-                isSearching.value = false
-                return@launch
-            }
-
-            val results = listToSearch.filter {
-                with(query.lowercase(Locale.getDefault()).trim()) {
-                    it.pokemonName.lowercase(Locale.getDefault()).contains(this)
-                            || it.number.toString() == this
-                }
-            }
-
-            if (!isSearching.value) {
-                cachedPokemonList = pokemonList.value
-                isSearching.value = true
-            }
-
-            pokemonList.value = results
-        }
+//        val listToSearch = if (isSearching.value) {
+//            cachedPokemonList
+//        } else {
+//            pokemonList.value
+//        }
+//
+//        viewModelScope.launch(Dispatchers.Default) {
+//            if (query.isBlank()) {
+//                pokemonList.value = cachedPokemonList
+//                isSearching.value = false
+//                return@launch
+//            }
+//
+//            val results = listToSearch.filter {
+//                with(query.lowercase(Locale.getDefault()).trim()) {
+//                    it.pokemonName.lowercase(Locale.getDefault()).contains(this)
+//                            || it.number.toString() == this
+//                }
+//            }
+//
+//            if (!isSearching.value) {
+//                cachedPokemonList = pokemonList.value
+//                isSearching.value = true
+//            }
+//
+//            pokemonList.value = results
+//        }
     }
 
     fun getDominantColor(drawable: Drawable): Color {
--- code/app/src/main/java/dev/tonholo/study/pokedex/screens/pokemonList/components/PokemonList.kt
+++ code/app/src/main/java/dev/tonholo/study/pokedex/screens/pokemonList/components/PokemonList.kt
@@ -4,47 +4,41 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.compose.collectAsLazyPagingItems
 import coil.annotation.ExperimentalCoilApi
 import dev.tonholo.study.pokedex.screens.pokemonList.PokemonListViewModel
 
+@ExperimentalPagingApi
 @ExperimentalCoilApi
 @Composable
 fun PokemonList(
     navController: NavController,
     viewModel: PokemonListViewModel = hiltViewModel(),
 ) {
-    val pokemonList by remember { viewModel.pokemonList }
-    val isEndReached by remember { viewModel.isEndReached }
+    val lazyPagingItems = viewModel.pokemonList.collectAsLazyPagingItems()
     val loadError by remember { viewModel.loadingError }
-    val isLoading by remember { viewModel.isLoading }
     val isSearching by remember { viewModel.isSearching }
 
     LazyColumn(contentPadding = PaddingValues(16.dp)) {
-        with(pokemonList) {
-            val itemCount = if (size % 2 == 0) {
-                size / 2
-            } else {
-                size / 2 + 1
-            }
-            items(itemCount) {
-                if (it >= itemCount - 1 && !isEndReached && !isLoading && !isSearching) {
-                    viewModel.loadPokemonListPaginated()
+        for (index in 0 until lazyPagingItems.itemCount step 2) {
+            item {
+                lazyPagingItems[index]?.let { first ->
+                    PokedexRow(
+                        first = first,
+                        last = lazyPagingItems[index + 1],
+                        navController = navController,
+                    )
                 }
-                PokedexRow(
-                    rowIndex = it,
-                    entries = this@with,
-                    navController = navController,
-                    viewModel = viewModel
-                )
             }
         }
     }
@@ -53,11 +47,11 @@ fun PokemonList(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize(),
     ) {
-        if (isLoading) {
-            CircularProgressIndicator(
-                color = MaterialTheme.colors.primary,
-            )
-        }
+//        if (isLoading) {
+//            CircularProgressIndicator(
+//                color = MaterialTheme.colors.primary,
+//            )
+//        }
 
         if (loadError.isNotBlank()) {
             Text(
